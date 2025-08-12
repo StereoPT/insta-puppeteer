dc5787995781f0dc861a3d86a6a25ba95439d667
StereoPT <stereopt@gmail.com>
2025-08-12 17:57:07 +0100

    chore: added biome

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,22 +1,24 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-import './globals.css';
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Insta-Puppeteer | Dashboard',
+  title: "Insta-Puppeteer | Dashboard",
 };
 
 type RootLayoutProps = Readonly<{
   children: React.ReactNode;
 }>;
 
-export default function RootLayout({ children }: RootLayoutProps) {
+const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
