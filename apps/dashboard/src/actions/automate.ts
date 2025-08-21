6e2bbdfd9c2bec0a0a1caa053b02c25ab4c3473c
StereoPT <stereopt@gmail.com>
2025-08-21 16:25:41 +0100

    fix: working solution

--- apps/dashboard/src/actions/automate.ts
+++ apps/dashboard/src/actions/automate.ts
@@ -1,28 +1,29 @@
 "use server";
 
-// import { InstagramAutomator } from "@insta-puppeteer/automator";
+import { InstagramAutomator } from "@insta-puppeteer/automator";
 
 type AutomateArgs = { email: string; password: string };
 
 export const Automate = async ({ email, password }: AutomateArgs) => {
-  console.log({ email, password });
-  // const config = {
-  //   profileName: "Something Here",
-  //   email,
-  //   password,
-  //   userAgent:
-  //     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-  //   headless: false,
-  //   viewport: { width: 1280, height: 860 },
-  // };
+  const config = {
+    profileName: "stereopt",
+    email,
+    password,
+    userAgent:
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    headless: false,
+    viewport: { width: 1280, height: 860 },
+  };
 
-  // const automator = new InstagramAutomator(config);
-  // await automator.initialize();
+  const automator = new InstagramAutomator(config);
+  await automator.initialize();
 
-  // await automator.login();
+  await automator.login();
 
-  // const posts = await automator.processHashtag("lifestyle", 10);
-  // console.log(`Processed ${posts.length} posts`);
+  const posts = await automator.processHashtag("football", 3);
+  console.log(`Processed ${posts.length} posts`);
+
+  await automator.close();
 
   return true;
 };
---
+++ packages/automator/src/index.ts
@@ -0,0 +1,1 @@
+export { InstagramAutomator } from "./InstagramAutomator";
