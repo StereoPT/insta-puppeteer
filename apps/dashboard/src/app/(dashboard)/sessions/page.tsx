791ed5862a0ff0f0e6cf4069448efb939c594581
StereoPT <stereopt@gmail.com>
2026-01-05 11:07:19 +0000

    refactor: session hydration

--- apps/dashboard/src/app/(dashboard)/sessions/page.tsx
+++ apps/dashboard/src/app/(dashboard)/sessions/page.tsx
@@ -1,14 +1,26 @@
+import { GetSessions } from "@/actions/sessions/getSessions";
 import { PageHeader } from "@/components/PageHeader";
 import { UserSessions } from "@/components/sessions/UserSessions";
+import { KEYS } from "@/constants/queryKeys";
+import { getQueryClient } from "@/lib/getQueryClient";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-const SessionsPage = () => {
+const SessionsPage = async () => {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: KEYS.sessions,
+    queryFn: () => GetSessions(),
+  });
+
   return (
-    <div className="flex flex-1 flex-col h-full">
-      <PageHeader description="List of your sessions" title="Sessions" />
-      <div className="h-full py-6">
-        <UserSessions />
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex flex-1 flex-col h-full">
+        <PageHeader description="List of your sessions" title="Sessions" />
+        <div className="h-full py-6">
+          <UserSessions />
+        </div>
       </div>
-    </div>
+    </HydrationBoundary>
   );
 };
 
