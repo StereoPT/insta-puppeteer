f0d568f6b518347bbb6d16384ea6ea620f1713dc
StereoPT <stereopt@gmail.com>
2026-01-06 10:45:29 +0000

    refactor: session post delete

--- apps/dashboard/src/actions/sessions/deleteSession.ts
+++ apps/dashboard/src/actions/sessions/deleteSession.ts
@@ -1,10 +1,20 @@
 "use server";
 
 import { prisma } from "@insta-puppeteer/database";
+import { rmSync } from "node:fs";
+import path from "node:path";
 
 export const DeleteSession = async (id: string) => {
-  const result = await prisma.session.delete({ where: { id } });
-  if (!result) {
+  const deletedSession = await prisma.session.delete({
+    where: { id },
+    include: { posts: true },
+  });
+
+  if (!deletedSession) {
     throw new Error("Failed to Delete Session");
+  }
+
+  for (const post of deletedSession.posts) {
+    rmSync(path.join("public", "images", `${post.id}.png`));
   }
 };
