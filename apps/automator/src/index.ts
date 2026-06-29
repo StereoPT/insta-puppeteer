805b83f7cf6bba1451bef351809ab1b04a5ae4f0
StereoPT <stereopt@gmail.com>
2026-06-29 15:34:23 +0100

    feat: automator express server

--- apps/automator/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { InstagramAutomator } from "./InstagramAutomator";
--- apps/automator/src/main.ts
+++ apps/automator/src/main.ts
@@ -1,45 +1,38 @@
 import "dotenv/config";
 
+import express from "express";
+
 import { InstagramAutomator } from "@insta-puppeteer/automator/InstagramAutomator";
 import { prisma } from "@insta-puppeteer/database/server";
 
-const config = {
-  profileName: "stereopt",
-  email: process.env.EMAIL ?? "",
-  password: process.env.PASSWORD ?? "",
-  userAgent:
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-  headless: false,
-  viewport: { width: 1280, height: 860 },
-};
+const app = express();
+const port = process.env.PORT || 3200;
 
-const main = async () => {
+app.use(express.json());
+
+app.post("/automate", async (req, res) => {
+  const { hashtag } = req.body;
+
+  const config = {
+    profileName: "stereopt",
+    email: process.env.EMAIL ?? "",
+    password: process.env.PASSWORD ?? "",
+    userAgent:
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    headless: false,
+    viewport: { width: 1280, height: 860 },
+  };
+
   const session = await prisma.session.create({
-    data: { status: "IN_PROGRESS" },
+    data: { status: "IN_PROGRESS", hashtag },
   });
 
   const automator = new InstagramAutomator(config);
+  automator.run(session.id, hashtag);
 
-  try {
-    await automator.initialize();
+  res.json({ sessionId: session.id });
+});
 
-    await automator.login();
-
-    await automator.processForYou(session.id, 8);
-
-    await prisma.session.update({
-      where: { id: session.id },
-      data: { status: "COMPLETED" },
-    });
-  } catch (error) {
-    console.error("Automation error:", error);
-    await prisma.session.update({
-      where: { id: session.id },
-      data: { status: "FAILED" },
-    });
-  } finally {
-    await automator.close();
-  }
-};
-
-main();
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
